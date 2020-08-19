51a7699db6d0822833c1db95a46e76930e5f27ab
paul-cruz <poljet12@gmail.com>
2020-08-18 21:34:21 -0500

    Error view fixed

--- oximeter-web/src/components/Error/index.js
+++ oximeter-web/src/components/Error/index.js
@@ -3,25 +3,34 @@ import { Link } from 'react-router-dom';
 import PageNotFound from "../../images/404_v2.png";
 import VerifyEmail from "../../images/Verify-email.png";
 import background from "../../images/fondo.jpg";
+import { makeStyles } from '@material-ui/core';
+import Page from "../Page";
 
-const back_style = {
-  backgroundImage: `url(${background})`,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundImage: `url(${background})`,
   backgroundPosition: 'center',
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
   height: '100vh',
   textAlign: 'center',
-}
+  }
+}));
+
 const Errors = {
   "404": PageNotFound,
   "Verify": VerifyEmail,
 };
 
 export default function Error({ err, msj }) {
-  return (<div className="root" style={back_style}>
-    <Link to="/home">
-      <img src={Errors[err]} alt={err} style={{ height: '50vh', margin: '5%' }} />
-    </Link>
-    <h2>{msj}</h2>
-  </div >);
+  const classes = useStyles();
+
+  return (
+    <Page className={classes.root} title="404">
+      <Link to="/home">
+        <img src={Errors[err]} alt={err} style={{ height: '50vh', margin: '5%' }} />
+      </Link>
+      <h2>{msj}</h2>
+    </Page >
+  );
 }
