fe110b2f3cc7fc044d2667b04f1ed3180fe7e105
paul-cruz <poljet12@gmail.com>
2020-08-18 21:47:36 -0500

    Global theme

---
+++ oximeter-web/src/theme/index.js
@@ -0,0 +1,27 @@
+import { createMuiTheme, colors } from '@material-ui/core';
+import shadows from './shadows';
+import typography from './typography';
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      dark: '#F4F6F8',
+      default: colors.common.white,
+      paper: colors.common.white
+    },
+    primary: {
+      main: colors.lightBlue[600]
+    },
+    secondary: {
+      main: colors.lightBlue[800]
+    },
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600]
+    }
+  },
+  shadows,
+  typography
+});
+
+export default theme;
