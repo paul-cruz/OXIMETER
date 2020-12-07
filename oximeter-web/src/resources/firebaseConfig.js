88075478fc4bcdfde47eb5872ca7b01f5268fcab
paul-cruz <poljet12@gmail.com>
2020-12-06 20:53:10 -0600

    Insert firebase initializeApp JSON

--- oximeter-web/src/resources/firebaseConfig.js
+++ oximeter-web/src/resources/firebaseConfig.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyB5oQOX_jfnnkXrUnhppKQcKM14pkkAc9A",
-    authDomain: "dev-oximeter.firebaseapp.com",
-    databaseURL: "https://dev-oximeter.firebaseio.com",
-    projectId: "dev-oximeter",
-    storageBucket: "dev-oximeter.appspot.com",
-    messagingSenderId: "742491490618",
-    appId: "1:742491490618:web:16279b19ce3fb9426c2803"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
