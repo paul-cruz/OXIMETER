0e72b5df3dec688a9bab39400ec15cbef6169859
Miguel Islas 2 <correo@correo.com>
2021-07-01 07:44:03 +0200

    Spliting ino and node-mcu projects

---
+++ ino-project/oximeter-node-mcu/esp8266_mqtt.h
@@ -0,0 +1,190 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// This file contains static methods for API requests using Wifi / MQTT
+
+#include <ESP8266WiFi.h>
+#include "FS.h"
+
+// You need to set certificates to All SSL cyphers and you may need to
+// increase memory settings in Arduino/cores/esp8266/StackThunk.cpp:
+//   https://github.com/esp8266/Arduino/issues/6811
+#include "WiFiClientSecureBearSSL.h"
+#include <time.h>
+
+#include <MQTT.h>
+
+#include <CloudIoTCore.h>
+#include <CloudIoTCoreMqtt.h>
+#include "secrets.h" // Wifi configuration here
+
+// !!REPLACEME!!
+// The MQTT callback function for commands and configuration updates
+// Place your message handler code here.
+void messageReceivedAdvanced(MQTTClient *client, char topic[], char bytes[], int length)
+{
+  if (length > 0){
+    Serial.printf("incoming: %s - %s\n", topic, bytes);
+  } else {
+    Serial.printf("0\n"); // Success but no message
+  }
+}
+///////////////////////////////
+
+// Initialize WiFi and MQTT for this board
+static MQTTClient *mqttClient;
+static BearSSL::WiFiClientSecure netClient;
+static BearSSL::X509List certList;
+static CloudIoTCoreDevice device(project_id, location, registry_id, device_id);
+CloudIoTCoreMqtt *mqtt;
+
+///////////////////////////////
+// Helpers specific to this board
+///////////////////////////////
+String getDefaultSensor()
+{
+  return "Wifi: " + String(WiFi.RSSI()) + "db";
+}
+
+String getJwt()
+{
+  // Disable software watchdog as these operations can take a while.
+  ESP.wdtDisable();
+  time_t iat = time(nullptr);
+  Serial.println("Refreshing JWT");
+  String jwt = device.createJWT(iat, jwt_exp_secs);
+  ESP.wdtEnable(0);
+  return jwt;
+}
+
+static void readDerCert(const char *filename) {
+  File ca = SPIFFS.open(filename, "r");
+  if (ca)
+  {
+    size_t size = ca.size();
+    uint8_t cert[size];
+    ca.read(cert, size);
+    certList.append(cert, size);
+    ca.close();
+
+    Serial.print("Success to open ca file ");
+  }
+  else
+  {
+    Serial.print("Failed to open ca file ");
+  }
+  Serial.println(filename);
+}
+
+static void setupCertAndPrivateKey()
+{
+  // Set CA cert on wifi client
+  // If using a static (pem) cert, uncomment in ciotc_config.h:
+  certList.append(primary_ca);
+  certList.append(backup_ca);
+  netClient.setTrustAnchors(&certList);
+
+  device.setPrivateKey(private_key);
+  return;
+
+  // If using the (preferred) method with the cert and private key in /data (SPIFFS)
+  // To get the private key run
+  // openssl ec -in <private-key.pem> -outform DER -out private-key.der
+
+  if (!SPIFFS.begin())
+  {
+    Serial.println("Failed to mount file system");
+    return;
+  }
+
+  readDerCert("/gtsltsr.crt"); // primary_ca.pem
+  readDerCert("/GSR4.crt"); // backup_ca.pem
+  netClient.setTrustAnchors(&certList);
+
+
+  File f = SPIFFS.open("/private-key.der", "r");
+  if (f) {
+    size_t size = f.size();
+    uint8_t data[size];
+    f.read(data, size);
+    f.close();
+
+    BearSSL::PrivateKey pk(data, size);
+    device.setPrivateKey(pk.getEC()->x);
+
+    Serial.println("Success to open private-key.der");
+  } else {
+    Serial.println("Failed to open private-key.der");
+  }
+
+  SPIFFS.end();
+}
+
+static void setupWifi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+  }
+
+  configTime(0, 0, ntp_primary, ntp_secondary);
+  Serial.println("Waiting on time sync...");
+  while (time(nullptr) < 1510644967)
+  {
+    delay(10);
+  }
+}
+
+///////////////////////////////
+// Orchestrates various methods from preceeding code.
+///////////////////////////////
+bool publishTelemetry(String data)
+{
+  return mqtt->publishTelemetry(data);
+}
+
+bool publishTelemetry(const char *data, int length)
+{
+  return mqtt->publishTelemetry(data, length);
+}
+
+bool publishTelemetry(String subfolder, String data)
+{
+  return mqtt->publishTelemetry(subfolder, data);
+}
+
+bool publishTelemetry(String subfolder, const char *data, int length)
+{
+  return mqtt->publishTelemetry(subfolder, data, length);
+}
+
+// TODO: fix globals
+void setupCloudIoT()
+{
+  // ESP8266 WiFi setup
+  setupWifi();
+
+  // ESP8266 WiFi secure initialization and device private key
+  setupCertAndPrivateKey();
+
+  mqttClient = new MQTTClient(512);
+  mqttClient->setOptions(180, true, 1000); // keepAlive, cleanSession, timeout
+  mqtt = new CloudIoTCoreMqtt(mqttClient, &netClient, &device);
+  mqtt->setUseLts(true);
+  mqtt->startMQTTAdvanced(); // Opens connection using advanced callback
+}
